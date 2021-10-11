46dffc0bce1d02383cddfe289f66da2e4e7854a7
gibberishxd <gogiberidzetornike8@gmail.com>
2021-10-11 13:26:08 +0300

    added cycle

---
+++ Lab3 var7/Lab3Var7.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+
+int main{
+	double x;
+	double a = x/(sqrt(k)*(k+2)
+	double sum = a
+	int k = 1;
+
+	while (!(k > 10 && abs(a) < 0.001))
+	{
+		k = k+1
+		a = x/(sqrt(k)*(k+2))
+	}
+
+}
