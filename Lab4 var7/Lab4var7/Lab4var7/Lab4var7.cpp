96fe07f05b3b5920c342a576ae021196ecd57fc4
gibberishxd <gogiberidzetornike8@gmail.com>
2021-11-05 09:20:22 +0200

    lab 4 finished

--- Lab4 var7/Lab4var7/Lab4var7/Lab4var7.cpp
+++ Lab4 var7/Lab4var7/Lab4var7/Lab4var7.cpp
@@ -5,16 +5,24 @@ using namespace std;
 
 int main()
 {
-    int n;
+    int n, i;
     cout << "Enter n (natural number) \n";
     cin >> n;
 
     double x1, x2, x3;
     x1 = 1;
     x2 = 0.3;
+    x3 = 0;
 
     cout << x1 << " = x1 \n";
     cout << x2 << " = x2 \n";
 
-
+    for (int i = 3; i <= n; i++ ) {
+        x3 = (i + 1) * x1;
+        x1 = x2;
+        x2 = x3;
+   }
+    cout << x3;
+    
+    return 0;
 }
