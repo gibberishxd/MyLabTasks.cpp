ec337b307806cd3a7b691b20aa6dd84414418e38
gibberishxd <gogiberidzetornike8@gmail.com>
2021-11-05 08:54:55 +0200

    lab 4 start of code

---
+++ Lab4 var7/Lab4var7/Lab4var7/Lab4var7.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter n (natural number) \n";
+    cin >> n;
+
+    double x1, x2, x3;
+    x1 = 1;
+    x2 = 0.3;
+
+    cout << x1 << " = x1 \n";
+    cout << x2 << " = x2 \n";
+
+
+}
