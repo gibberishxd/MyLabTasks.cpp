6cc96a75c6c6d5e9d8367413502d4f3104f2f023
gibberishxd <gogiberidzetornike8@gmail.com>
2021-11-12 12:22:38 +0200

    lab5 done

---
+++ Lab5var7/Lab5var7/Lab5var7.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int limit = 1000;
+
+    for (int i = 1; i <= limit; i++) {
+
+        unsigned int curNum = i;
+
+        int reNum = 0;
+
+        int quantity = 0;
+
+        while (curNum != 0) {
+            quantity++;
+            curNum = curNum / 10;
+        }
+
+        curNum = i;
+
+        for (int j = quantity - 1; j >= 0; j--) {
+            reNum = reNum + ((curNum % 10) * pow(10, j));
+            curNum = curNum / 10;
+        }
+
+        if (i == reNum) {
+            cout << i;
+        }
+    }
+    return 0;
+}
